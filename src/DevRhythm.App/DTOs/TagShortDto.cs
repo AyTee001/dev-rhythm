6da36c1bf09b0d87b4039cc56f107393f79c40de
AyTee001 <tiutiunanastasia@gmail.com>
2023-12-18 13:43:32 +0200

    fix tag dto

--- src/DevRhythm.App/DTOs/TagShortDto.cs
+++ src/DevRhythm.App/DTOs/TagShortDto.cs
@@ -2,8 +2,9 @@
 
 namespace DevRhythm.App.DTOs
 {
-    public class TagShortDto : Entity
+    public class TagShortDto
     {
+        public long Id { get; set; }
         public string? Name { get; set; }
     }
 }
