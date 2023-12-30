687f758779783594af87b82a05bbf9d47bf3ffd5
AyTee001 <tiutiunanastasia@gmail.com>
2023-12-30 14:58:15 +0200

    Add hub

---
+++ src/DevRhythm.Web/Hubs/Interfaces/INotificationHub.cs
@@ -0,0 +1,9 @@
+﻿using DevRhythm.Core.Entities;
+
+namespace DevRhythm.Infrastructure.Hubs.Interfaces
+{
+    public interface INotificationHub
+    {
+        Task SendNotificationAsync(Notification notification);
+    }
+}
---
+++ src/DevRhythm.Web/Hubs/NotificationHub.cs
@@ -0,0 +1,19 @@
+﻿using DevRhythm.Infrastructure.Hubs.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace DevRhythm.Infrastructure.Hubs
+{
+    public sealed class NotificationHub : Hub<INotificationHub>
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(!userId.IsNullOrEmpty())
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, userId!);
+            }
+        }
+    }
+}
