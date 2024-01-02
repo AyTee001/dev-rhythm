c368b40d3a06ded18bf4a5e88a8435a514a550dd
AyTee001 <tiutiunanastasia@gmail.com>
2024-01-02 16:36:56 +0200

    Cleaned frontend for notifications

--- src/DevRhythm.Web/wwwroot/js/notification.js
+++ src/DevRhythm.Web/wwwroot/js/notification.js
@@ -1,3 +1,19 @@
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl, option)
+});
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/notificationHub")
+    .build();
+
+
+connection.on("SendNotificationAsync", (notification) => {
+    notificationReceived(notification);
+});
+
+connection.start().catch(() => console.log("Error"));
+
 function notificationReceived(notification) {
     $.ajax({
         url: '/Notifications/RenderNotification',
@@ -5,10 +21,19 @@ function notificationReceived(notification) {
         contentType: 'application/json',
         data: JSON.stringify(notification),
         success: function (data) {
-            console.log(data);
-            $('body').append(data);
-            $('.toast').toast({ autohide: true, delay: 5000 });
-            $('.toast').toast('show');
+            var $toast = $(data).appendTo(".toast-container");
+
+            var closeButton = $toast.find('.close');
+            closeButton.on('click', function () {
+                $toast.remove();
+            });
+
+            $toast.toast({
+                autohide: true,
+                delay: 5000
+            }).toast('show').on('hidden.bs.toast', function () {
+                $(this).remove();
+            });
         },
         error: function (err) {
             console.log(err);
